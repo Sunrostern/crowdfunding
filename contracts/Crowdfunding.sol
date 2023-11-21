ab852010f7ec4777d2ee7572f5b5e4d30fe50347
Vladimir Ten <sunrostern@gmail.com>
2023-11-20 21:30:16 -0700

    Crowdfunding Logic

--- contracts/Crowdfunding.sol
+++ contracts/Crowdfunding.sol
@@ -13,6 +13,62 @@ THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR I
 
 pragma solidity ^0.8.19;
 
+import "./Token.sol";
+
 contract Crowdfunding {
-  string public name = "Crowdfunding";
+  address public owner;
+  Token public token;
+  uint256 public price;
+  uint256 public maxTokens;
+  uint256 public tokensSold;
+
+  event Buy(
+    uint256 amount,
+    address buyer
+  );
+
+  event Finalize(
+    uint256 tokensSold,
+    uint256 ethRaised
+  );
+
+  constructor(Token _token, uint256 _price, uint256 _maxTokens) {
+    owner = msg.sender;
+    token = _token;
+    price = _price;
+    maxTokens = _maxTokens;
+  }
+
+    modifier onlyOwner() {
+      require(msg.sender == owner, "Caller is not the Owner.");
+      _;
+    }
+
+  receive() external payable {
+    uint256 amount = msg.value / price;
+    buyTokens(amount * 1e18);
+  }
+
+  function buyTokens(uint256 _amount) public payable {
+    require(msg.value == (_amount / 1e18) * price);
+    require(token.balanceOf(address(this)) >= _amount);
+    require(token.transfer(msg.sender, _amount));
+    tokensSold += _amount;
+    emit Buy(_amount, msg.sender);
+  }
+
+  function setPrice(uint256 _price) public onlyOwner {
+    price = _price;
+  }
+
+  function finalize() public onlyOwner {
+    // Sending Ethers to the Crowdfunding Creator.
+    require(token.transfer(owner, token.balanceOf(address(this))));
+
+    // Sending any other remaining Tokens to the Crowdfunding Creator.
+    uint256 value = address(this).balance;
+    (bool sent, ) = owner.call{ value: value }("");
+    require(sent);
+    emit Finalize(tokensSold, value);
+  }
 }
--- test/Crowdfunding.js
+++ test/Crowdfunding.js
@@ -2,20 +2,164 @@ const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+  return ethers.utils.parseUnits(n.toString(), 'ether');
+};
+const ether = tokens;
 
 describe('Crowdfunding', () => {
-  let CrowdfundingContract
+  let accounts,
+    CrowdfundingContract,
+    deployer,
+    TokenContract,
+    user1;
 
   beforeEach(async () => {
-    const CrowdfundingContractFactory = await ethers.getContractFactory('Crowdfunding')
-    CrowdfundingContract = await CrowdfundingContractFactory.deploy()
-  })
-  
+    // Initializing Contract Factories.
+    const CrowdfundingContractFactory = await ethers.getContractFactory('Crowdfunding');
+    const TokenContractFactory = await ethers.getContractFactory('Token');
+
+    // Deploying Contracts.
+    TokenContract = await TokenContractFactory.deploy('Token', 'TOK', 1000000);
+    accounts = await ethers.getSigners();
+    deployer = accounts[0];
+    user1 = accounts[1];
+    CrowdfundingContract = await CrowdfundingContractFactory.deploy(TokenContract.address, ether(1), ether(1000000));
+
+    // Seeding Tokens into the Crowdfunding Contract.
+    let transaction = await TokenContract.connect(deployer).transfer(CrowdfundingContract.address, tokens(1000000));
+    await transaction.wait();
+  });
+
   describe('Deployment', () => {
-    it('Has a correct name.', async () => {
-      expect(await CrowdfundingContract.name()).to.equal('Crowdfunding')
+    it('Returns Token price.', async () => {
+      expect(await CrowdfundingContract.price()).to.equal(ether(1));
+    });
+
+    it('Returns the correct max Tokens.', async () => {
+      expect(await CrowdfundingContract.maxTokens()).to.equal(ether(1000000));
+    });
+
+    it('Returns Token address.', async () => {
+      expect(await CrowdfundingContract.token()).to.equal(TokenContract.address);
+    });
+
+    it('Seeds Tokens into the Crowdfunding Contract.', async () => {
+      expect(await TokenContract.balanceOf(CrowdfundingContract.address)).to.equal(tokens(1000000));
     })
-  })
-})
+  });
+
+  describe('Buying Tokens', () => {
+    const amount = tokens(10);
+    let result, transaction;
+    describe('Success', () => {
+      beforeEach(async () => {
+        transaction = await CrowdfundingContract.connect(user1).buyTokens(amount, { value: ether(10) });
+        result = await transaction.wait();
+      });
+
+      it('Transfers Tokens.', async () => {
+        expect(await TokenContract.balanceOf(CrowdfundingContract.address)).to.equal(tokens(999990));
+        expect(await TokenContract.balanceOf(user1.address)).to.equal(amount);
+      });
+
+      it('Updates Contract Ether balance.', async () => {
+        expect(await ethers.provider.getBalance(CrowdfundingContract.address)).to.equal(amount);
+      });
+
+      it('Updates Tokens sold.', async () => {
+        expect(await CrowdfundingContract.tokensSold()).to.equal(amount);
+      });
+
+      it('Emits a Buy Event.', async () => {
+        await expect(transaction).to.emit(CrowdfundingContract, 'Buy').withArgs(amount, user1.address);
+      });
+    });
+
+    describe('Failure', () => {
+      it('Rejects insufficient Ethers.', async () => {
+        await expect(CrowdfundingContract.connect(user1).buyTokens(tokens(10), { value: 0 })).to.be.reverted;
+      });
+    });
+  });
+
+  describe('Sending Ethers', () => {
+    const amount = ether(10);
+    let result, transaction;
+
+    describe('Success', () => {
+      beforeEach(async () => {
+        transaction = await user1.sendTransaction({
+          to: CrowdfundingContract.address,
+          value: amount
+        });
+      });
+
+      it('Updates Contract Ethers value.', async () => {
+        expect(await ethers.provider.getBalance(CrowdfundingContract.address)).to.equal(amount);
+      });
+
+      it('Updates User Token balance.', async () => {
+        expect(await TokenContract.balanceOf(user1.address)).to.equal(amount);
+      });
+    });
+  });
+
+  describe('Updating Price', () => {
+    const price = ether(2);
+    let result, transaction;
+
+    describe('Success', () => {
+      beforeEach(async () => {
+        transaction = await CrowdfundingContract.connect(deployer).setPrice(price);
+        result = await transaction.wait();
+      });
+
+      it('Updates the price.', async () => {
+        expect(await CrowdfundingContract.price()).to.equal(price);
+      });
+    });
+
+    describe('Failure', () => {
+      it('Prevents a non-Owner from setting price.', async () => {
+        await expect(CrowdfundingContract.connect(user1).setPrice(price)).to.be.reverted;
+      });
+    });
+  });
+
+  describe('Finalizing Funding', () => {
+    const amount = tokens(10);
+    const value = ether(10);
+    let result, transaction;
+
+    describe('Success', () => {
+      beforeEach(async () => {
+        transaction = await CrowdfundingContract.connect(user1).buyTokens(amount, {
+          value
+        });
+        result = await transaction.wait();
+
+        transaction = await CrowdfundingContract.connect(deployer).finalize();
+        result = await transaction.wait();
+      });
+
+      it('Transfers the remaining Tokens to the Owner.', async () => {
+        expect(await TokenContract.balanceOf(CrowdfundingContract.address)).to.equal(0);
+        expect(await TokenContract.balanceOf(deployer.address)).to.equal(tokens(999990));
+      });
+
+      it('Transfers the remaining Ethers balance to the Owner.', async () => {
+        expect(await ethers.provider.getBalance(CrowdfundingContract.address)).to.equal(0);
+      });
+
+      it('Emits a Finalize Event.', async () => {
+        await expect(transaction).to.emit(CrowdfundingContract, 'Finalize').withArgs(amount, value);
+      });
+    });
+
+    describe('Failure', () => {
+      it('Prevents a non-Owner from finalizing the crowdfunding.', async () => {
+        await expect(CrowdfundingContract.connect(user1).finalize()).to.be.reverted;
+      });
+    });
+  });
+});
